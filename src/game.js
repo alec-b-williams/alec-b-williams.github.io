d26d506896132a9a56f9071b2f903be69f656372
alec-b-williams <alec.b.williams@wsu.edu>
2022-06-24 15:32:41 -0700

    Work on capturing input

--- src/game.js
+++ src/game.js
@@ -1,12 +1,22 @@
 import GameDisplay from "./display.js";
 import Player from "./player.js";
+import { Map } from "../lib/index.js";
 
 class Game {
   constructor() {
-    this.gd = new GameDisplay(80, 40, this);
+    this.screenDim = {
+      width: 80,
+      height: 40
+    }
+    this.gd = new GameDisplay(this);
     this.player = new Player(40, 20);
+    this.map = new Map.Digger(this.screenDim.width, this.screenDim.height);
 
     this.gd.init();
+  }
+
+  run() {
+    
   }
 }
 
